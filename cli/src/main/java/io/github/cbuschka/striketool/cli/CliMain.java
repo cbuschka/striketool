1af2d4b2dc4fdbbb73155cdc873a8713d9ae9aa1
conni <cbuschka@gmail.com>
2024-02-04 19:52:17 +0100

    Adjusted start script, added cli module.

---
+++ cli/src/main/java/io/github/cbuschka/striketool/cli/CliMain.java
@@ -0,0 +1,11 @@
+package io.github.cbuschka.striketool.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliMain {
+    public static void main(String[] args) {
+        SpringApplication.run(CliMain.class, args);
+    }
+}
